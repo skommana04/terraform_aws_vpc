5ffd9d4356f73d5ba937a9b6e53044252967b456
skommana04 <skommana04@gmail.com>
2026-03-23 12:48:10 -0500

    a

--- variables.tf
+++ variables.tf
@@ -18,15 +18,15 @@ variable "environment" {
 variable "vpc_tags" {}
 
 variable "public_cidr_blocks" {
-    type = string
+    type = list(string)
     default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 variable "private_cidr_blocks" {
-    type = string
+    type = list(string)
      default = ["10.0.10.0/24","10.0.20.0/24"]
 }
 variable "database_cidr_blocks" {
-    type = string
+    type = list(string)
     default = ["10.0.11.0/24","10.0.22.0/24"]
 }
 
